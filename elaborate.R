c8e17c80f6c2ff436152e32337a8d34c497b7043
doctgig <lucabasilico.it@gmail.com>
2019-11-12 15:24:36 +0100

    WIP

--- elaborate.R
+++ elaborate.R
@@ -4,9 +4,11 @@ library(dplyr)
 
 # load and read CSV data 
 google_csv <- read.csv(file = "google-output.csv", header = TRUE, sep = ",")
+azure_csv <- read.csv(file = "azure-output.csv", header = TRUE, sep = ",")
 
-ids <- google_csv %>% 
-    group_by(image.name) %>%
-    summarize(n())
-  
-ids
+#google <- google_csv %>% 
+#    group_by(image.name) %>%
+#    summarize(n())
+
+data <- merge(x = google_csv, y = azure_csv,by = c("image.name", "image.location", "image.publicUri")) %>%
+    group_by(image.name)
